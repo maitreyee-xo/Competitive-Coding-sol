bffe88ad9c7f140262af22000a306cd107c67d22
Maitreyee Singh <86715433+maitreyee-xo@users.noreply.github.com>
2022-07-24 12:34:31 +0530

    Update TwoMax.java

--- TwoMax.java
+++ TwoMax.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 class TwoMax {
     public int maximumProduct(int[] nums) {
         Arrays.sort(nums);
-        return (nums[nums.length - 1] * nums[nums.length - 2]
+        return MAth.max(nums[0] * nums[1]* nums[2],nums[nums.length - 1] * nums[nums.length - 2]
                         * nums[nums.length - 3]); //
     }
 }
