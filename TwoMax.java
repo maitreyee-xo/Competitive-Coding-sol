eb86734e650407256b8550261e61561fe0d83ed1
Maitreyee Singh <86715433+maitreyee-xo@users.noreply.github.com>
2022-07-24 12:02:14 +0530

    Add files via upload

---
+++ TwoMax.java
@@ -0,0 +1,10 @@
+package practiseCoding;
+import java.util.Arrays;
+class TwoMax {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        return Math.max(nums[0] * nums[1] * nums[nums.length - 1],
+                nums[nums.length - 1] * nums[nums.length - 2]
+                        * nums[nums.length - 3]);
+    }
+}
