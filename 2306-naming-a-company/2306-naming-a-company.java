54b164f54abaeb0172e8b7421c7d2192bd4ed78b
Maitreyee Singh <86715433+maitreyee-xo@users.noreply.github.com>
2023-02-09 17:20:35 +0530

    Time: 177 ms (92.06%), Space: 53.5 MB (69.84%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long distinctNames(String[] ideas) {
+        long validSelection = 0;
+        HashSet<String> [] grp = new HashSet[26];
+        for(int i = 0; i < 26;i++)
+        {
+            grp[i] = new HashSet<>();
+        }
+        for(String idea : ideas)
+        {
+            grp[idea.charAt(0) - 'a'].add(idea.substring(1));
+        }
+        for(int i = 0; i < 25;++i)
+        {
+            for(int j = i+1; j < 26;j++)
+            {
+                long common = 0;
+                for(String str : grp[i])
+                {
+                    if(grp[j].contains(str)) common++;
+                }
+                validSelection = validSelection + 2 * (grp[i].size() - common) * (grp[j].size() - common);
+            }
+        }
+        return validSelection;
+}
+}
