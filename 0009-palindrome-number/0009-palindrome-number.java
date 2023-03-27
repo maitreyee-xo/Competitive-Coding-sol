c8ad188fce3cae61feb29a0b6a4dfa98b7ebfe16
Maitreyee Singh <86715433+maitreyee-xo@users.noreply.github.com>
2023-03-27 14:53:38 +0530

    Time: 10 ms (51.32%), Space: 41.9 MB (49.84%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,19 +1,8 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0) return false;
-        Stack<Integer> stk = new Stack<>();
-        int num = x;
-        while(num > 0)
-        {
-            stk.push(num % 10);
-            num /= 10;
-        }
-        num = x;
-        while(!stk.isEmpty())
-        {
-            if(num % 10 != stk.pop()) return false;
-            num /= 10;
-        }
+        if (x<0 || (x!=0 && x%10==0)) return false;
+        String str = String.valueOf(x);
+        for(int i = 0; i < str.length() / 2;i++)  if (str.charAt(i) != str.charAt(str.length()-i-1)) return false;
         return true;
     }
 }
