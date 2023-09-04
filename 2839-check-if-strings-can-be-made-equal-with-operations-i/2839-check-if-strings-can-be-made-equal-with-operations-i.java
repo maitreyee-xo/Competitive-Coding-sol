9974bf4a521bce8b0e0afa29f9a4001e61c23549
Maitreyee Singh <86715433+maitreyee-xo@users.noreply.github.com>
2023-09-04 11:26:19 +0530

    Time: 1 ms (100.00%), Space: 41.3 MB (100.00%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;        
+        for(int i = 0; i < s1.length(); i++)  if(s1.charAt(i) != s2.charAt(i) && s1.charAt(i) != s2.charAt((i + 2) % s1.length())) return false;
+        return true;
+    }
+}
