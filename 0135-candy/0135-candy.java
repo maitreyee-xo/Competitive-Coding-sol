6725809c03aa59a97b8e47cd6b2f14df01086465
Maitreyee Singh <86715433+maitreyee-xo@users.noreply.github.com>
2023-09-13 12:05:14 +0530

    Time: 3 ms (79.95%), Space: 43.7 MB (64.56%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,19 +1,15 @@
-public class Solution {
+class Solution {
     public int candy(int[] ratings) {
-        int[] candies = new int[ratings.length];
+        int [] candies = new int[ratings.length];
         Arrays.fill(candies, 1);
-        for (int i = 1; i < ratings.length; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
-            }
+        for(int i=1; i<ratings.length; i++){
+            if(ratings[i]>ratings[i-1]) candies[i] = candies[i-1]+1;
         }
-        int sum = candies[ratings.length - 1];
-        for (int i = ratings.length - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
-            }
-            sum += candies[i];
+        for(int i=ratings.length-2; i>=0; i--){
+            if(ratings[i]>ratings[i+1]) candies[i] = Math.max(candies[i], candies[i+1]+1);
         }
-        return sum;
+        int count=0;
+        for(int i:candies) count+=i;
+        return count;
     }
 }
