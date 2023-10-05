acf04ee24a231969cc8ef0d95c700f29673fec7d
Maitreyee Singh <86715433+maitreyee-xo@users.noreply.github.com>
2023-10-05 12:28:52 +0530

    Time: 11 ms (40.97%), Space: 46.8 MB (38.25%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -4,10 +4,7 @@ class Solution {
         List<Integer> arr = new ArrayList<>();
         int check = (int) Math.floor(nums.length / 3.0);
         for(int i : nums) hm.put(i, hm.getOrDefault(i, 0)+1);
-        for(int x : hm.keySet())
-        {
-            if(hm.get(x) > check) arr.add(x);
-        }
+        for(int x : hm.keySet())    if(hm.get(x) > check) arr.add(x);
         return arr;
     }
 }
