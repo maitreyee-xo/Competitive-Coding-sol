23bec8fd197f54e6deeb1b59b7c9ec4ad8fb2acd
Maitreyee Singh <86715433+maitreyee-xo@users.noreply.github.com>
2023-10-06 19:52:20 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (28.84%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,15 +1,8 @@
 class Solution {
     public int integerBreak(int n) {
-        if (n <= 3)  return n - 1; 
-        int[] dp = new int[n + 1];
-        for (int i = 1; i <= 3; i++)   dp[i] = i;
-        for (int num = 4; num <= n; num++) {
-            int ans = num;
-            for (int i = 2; i < num; i++) {
-                ans = Math.max(ans, i * dp[num - i]);
-            }            
-            dp[num] = ans;
-        }
-        return dp[n];
+        if (n <= 3)   return n - 1;        
+        if (n % 3 == 0) return (int) Math.pow(3, n / 3);
+        if (n % 3 == 1)  return (int) Math.pow(3, (n / 3 - 1)) * 4;
+        return (int) Math.pow(3, n / 3) * 2;
     }
 }
